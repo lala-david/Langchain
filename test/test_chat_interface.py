b83f70c36e7c731b5ef3e208cdfdbf1c8a9790fc
lala-david <wnswns1946@naver.com>
2023-11-19 02:34:01 +0900

    ♾ Test  #3

--- test/test_chat_interface.py
+++ test/test_chat_interface.py
@@ -1,6 +1,9 @@
 import unittest
 from chat_interface import setup_chatbot
 from data_loader import load_and_process_pdf_data
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
 
 class TestSetupChatbot(unittest.TestCase):
     def test_setup_chatbot(self):
